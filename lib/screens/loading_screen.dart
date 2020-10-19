2ddc878832ed216fa27f3cc0d185140857603a2d
Alex Molina <alex98criado@gmail.com>
2020-10-19 16:15:01 +0200

    learning about http package and jsonDecoder of dart convert

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:geoclima/utilities/constants.dart';
 import 'package:geoclima/services/Location.dart';
+import 'package:http/http.dart' as http; //para usar todo lo que hay en el paquete con hhtp
+import 'dart:convert';
 
 
 class LoadingScreen extends StatefulWidget {
-
   //get the location of the current user
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -12,11 +13,45 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-
-  void getLocation() async{
+  /*
+  * Obtener latitud y longitud del dispositivo
+  * usando getCurrentLocation de la clase Latitude de geolocator package
+  * */
+  void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
     print("Latitud: ${location.latitude} , Longitud ${location.longitude}");
+  }
+
+  /*
+  * Obtener datos de openweather API
+  *
+  * */
+
+  void getData() async {
+    String data ;
+    //El metodo devuelve un Future<Response>
+    http.Response response = await http.get('http://api.openweathermap.org/data/2.5/weather?lat=37.207207207207205&lon=-3.619648330507012&mode=json&appid=900b943bb1d48f5e785c8a1dc5a0be91',);
+
+    if(response.statusCode == 200) {
+      /*
+      * El resultado del request es un json
+      * es decir , los datos están en formato {"key" : value}
+      * */
+      data = response.body;
+      print(data);
+
+      var temp = jsonDecode(data) ['main']['temp']; // temperatura
+      var condition_number = jsonDecode(data)['weather'][0]['id']; //condition_number
+      var city_name = jsonDecode(data)['name']; //city name
+
+      print('temperatura: $temp\ncondition number: $condition_number\nnombre ciudad: $city_name');
+      print('temperatura: $temp\ncondition number: $condition_number\nnombre ciudad: $city_name');
+
+    }else {
+      print(response.statusCode);
+    }
+
   }
 
   /*
@@ -24,17 +59,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
   * Al iniciar el widget , obtenemos posición del dispositivo
   * */
   @override
-  void initState(){
+  void initState() {
     getLocation();
+    getData();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-
-
-    return Scaffold(
-
-    );
+    return Scaffold(body: Container());
   }
 }
--- lib/services/Location.dart
+++ lib/services/Location.dart
@@ -13,17 +13,27 @@ class Location {
 
 
   //obtener la posicion actual del usuario
-  void getCurrentLocation() async{
+  Future<void> getCurrentLocation() async{
+
     try {
       Position position = await getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+
       latitude = position.latitude;
       longitude = position.longitude;
+
     }catch(e){
-      latitude = 8.0;
-      longitude = 8.0;
+      latitude = 0.0;
+      longitude = 0.0;
     }
 
 
+  }
+
+
+  //lanza una excepcion si el arg es >= 10
+  void _dummyError(int dummyNumber){
+    if (dummyNumber >= 10)
+      throw FormatException("MAL HECHO");
   }
 
 }
